db64888ae46d52dae7ac69a2d90ea1f60bb821b0
JEDelanois <jedelanois@gmail.com>
2016-04-02 12:33:14 -0500

    These files can currently write to a google spreadsheet

---
+++ TembooAccount.h
@@ -0,0 +1,7 @@
+#define TEMBOO_ACCOUNT "erikd"  // your Temboo account name 
+#define TEMBOO_APP_KEY_NAME "ECE110Project"  // your Temboo app name
+#define TEMBOO_APP_KEY  "0476960948a14be88ae8b6e5e0063b8f"  // your Temboo app key
+
+#define WIFI_SSID "HOME-50E2"
+#define WIFI_PASSWORD "B13900120CB79E62"
+
